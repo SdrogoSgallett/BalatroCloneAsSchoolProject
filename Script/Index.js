f6dc61bf9c32795ed25df0b7b3f78e210b24c4c3
SdrogoSgallett <paolo.zuttion.stud@ispascalcomandini.it>
2023-11-15 13:21:24 +0100

    mezzo casino - sfondi carte

--- Script/Index.js
+++ Script/Index.js
@@ -2,16 +2,21 @@ function GeneraCarta(){
     const GameArea = document.getElementById("GameArea");
     const Carta = document.createElement("div");
     Carta.setAttribute("class","Card");
-    const suits = document.createElement("div");
+    const backgroundimage = document.createElement("img");
+    backgroundimage.setAttribute("class","immage");
+    backgroundimage.setAttribute("src","ASSO DI FIORI.jpg");
+    Carta.appendChild(backgroundimage);
+   /* const suits = document.createElement("div");
     suits.setAttribute("class","Black suit");
     suits.innerText = "♣";
     const value = document.createElement("div");
     value.setAttribute("class","Black rank");
     value.innerText = "10";
     Carta.appendChild(suits);
-    Carta.appendChild(value);
+    Carta.appendChild(value);*/
     GameArea.appendChild(Carta);
 }
 function OnLoad(){
+    GeneraCarta();
     GeneraCarta();
 }
